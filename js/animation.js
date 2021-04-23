6c4331ec85e753e1ea335edfa706a4d4ce5e2246
unknown <rogerthehedhog.98@gmail.com>
2021-04-23 12:23:17 -0500

    first

---
+++ js/animation.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal()
+    sr.reveal('.portafolio',{
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    })
+    sr.reveal('.about-services',{
+        duration: 3000,
+        origin: 'left',
+        distance: '-100px'
+    })
